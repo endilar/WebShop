80cd23c8d3ccb209560ec2d4b37746bf42fdbecc
endilar29 <endilar29@gmail.com>
2021-03-24 03:13:42 +0200

    ordervm

---
+++ WebShop/Controllers/OrderController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop_DataAccess.Repository.IRepository;
+using WebShop_Models.ViewModels;
+using WebShop_Utility;
+using WebShop_Utility.BrainTree;
+
+namespace WebShop.Controllers
+{
+    [Authorize(Roles = WC.AdminRole)]
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
+
+        public OrderController(IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo,
+            IBrainTreeGate brain)
+        {
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+            _brain = brain;
+        }
+
+
+        public IActionResult Index(string searchName = null, string searchEmail = null, string searchPhone = null, string Status = null)
+        {
+            OrderListVM orderListVM = new OrderListVM()
+            {
+                OrderHList = _orderHRepo.GetAll(),
+                StatusList = WC.listStatus.ToList().Select(i => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.FullName.ToLower().Contains(searchName.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchEmail))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.Email.ToLower().Contains(searchEmail.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchPhone))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.PhoneNumber.ToLower().Contains(searchPhone.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(Status) && Status != "--Order Status--")
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.OrderStatus.ToLower().Contains(Status.ToLower()));
+            }
+            return View(orderListVM);
+        }
+
+
+
+        public IActionResult Details(int id)
+        {
+            OrderVM = new OrderVM
+            {
+                OrderHeader = _orderHRepo.FirstOrDefault(u => u.Id == id),
+                OrderDetail = _orderDRepo.GetAll(o => o.OrderHeaderId == id, includeProperties: "Product")
+            };
+            return View(OrderVM);
+        }
+
+    }
+
+}
